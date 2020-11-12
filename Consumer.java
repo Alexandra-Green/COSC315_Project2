7f393aa92ce25383134e42249ac061a9fea8c11c
Jordan-Bao <45648932+Jordan-Bao@users.noreply.github.com>
2020-11-11 23:08:46 -0500

    Add files via upload

---
+++ Consumer.java
@@ -0,0 +1,53 @@
+import java.time.LocalTime;
+import java.util.*;
+
+public class Consumer extends Thread{
+int cID;
+LinkedList<Buffer> queue;
+
+Consumer(){
+	
+}
+
+Consumer(int cID, LinkedList<Buffer> queue){
+	this.cID = cID;
+	this.queue = queue;
+}
+
+public void run() {
+	while(true) {
+		Buffer request = new Buffer();
+		request = null;
+		if(queue.size()>0) 
+			synchronized(queue) {
+				request = queue.peek();
+				queue.remove();
+				queue.notifyAll(); //Make sure producer knows new space is available
+			}
+		//Leave lock so others consumers can work
+		if(request!=null) {
+			System.out.println("Consumer " + cID + ": assigned request ID " + request.rID + ", processing request for the next " + request.length/1000 + " seconds, current time is " + LocalTime.now());
+			try {
+				Thread.sleep(request.length);
+				System.out.println("Consumer " + cID + ": completed request ID " + request.rID + " at time " + LocalTime.now());
+			}
+			catch (InterruptedException e) {
+				System.out.println("Consumer " + cID + " cancelled request ID " + request.rID + ", current time is " + LocalTime.now());
+			}
+		}
+	if(queue.size()==0)
+		synchronized(queue) {
+	while(queue.size()==0) {
+		queue.notifyAll(); //Make sure producer wakes up
+		try {
+			queue.wait();
+		} catch (InterruptedException e) {
+		}
+	}
+	
+		
+	}
+	
+}
+}
+}
